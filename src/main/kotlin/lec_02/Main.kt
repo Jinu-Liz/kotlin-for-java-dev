33a1dcb0946fef9355a62e92451baeccf4aa633e
Hyungseock-Yun <bagzein@gmail.com>
2024-01-04 00:07:03 +0900

    - 코틀린에서 null을 다루는 방법 > Elvis 연산자 > 함수 리팩터링

--- src/main/kotlin/lec_02/Main.kt
+++ src/main/kotlin/lec_02/Main.kt
@@ -5,6 +5,12 @@ fun main() {
     val str: String? = "ABC"
     str.length  // 불가능
     str?.length // 가능
+
+    val str2: String? = null
+    println(str2?.length) // safe call은 null이 아니면 뒤에 걸 실행, null이면 전체 값이 null이 됨.
+
+    val str3: String? = null
+    println(str3?.length ?: 0) // Elvis 연산자. (앞의 연산 결과가 null이면 뒤의 값을 사용)
 }
 
 fun startsWithA1(str: String?): Boolean {
@@ -15,14 +21,28 @@ fun startsWithA1(str: String?): Boolean {
     return str.startsWith("A")
 }
 
+fun startsWithA1_kt(str: String?): Boolean {
+    return str?.startsWith("A") ?: throw IllegalArgumentException("null이 들어왔습니다")
+}
+
 fun startsWithA2(str: String?): Boolean? {
     if (str == null) return  null
 
     return str.startsWith("A")
 }
 
+fun startsWithA2_kt(str: String?): Boolean? {
+
+    return str?.startsWith("A")
+}
+
 fun startsWithA3(str: String?): Boolean {
     if (str == null) return false
 
     return str.startsWith("A")   // 위에 null을 감지하는 로직이 없으면, str에 null이 들어갈 수 있으므로 에러 발생 (safe call)
+}
+
+fun startsWithA3_kt(str: String?): Boolean {
+
+    return str?.startsWith("A") ?: false
 }
