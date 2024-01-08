39cbe1bdcfafb1c699b977b2181bbbb738c53b2a
Hyungseock-Yun <bagzein@gmail.com>
2024-01-08 23:57:01 +0900

    - 코틀린에서 함수를 다루는 방법 > kotlin에서 기본값을 이용한 함수 사용

--- src/main/kotlin/lec_08/Main.java
+++ src/main/kotlin/lec_08/Main.java
@@ -10,4 +10,26 @@ public class Main {
 
     return b;
   }
+
+  /**
+   * java에서는 overloading을 사용하여 기본값을 셋팅한 후, 사용
+   */
+  public void repeat(String str, int num, boolean useNewLine) {
+    for (int i = 1; i <= num; i++) {
+      if (useNewLine) {
+        System.out.println(str);
+      } else {
+        System.out.print(str);
+      }
+    }
+  }
+
+  // overloading
+  public void repeat(String str, int num) {
+    repeat(str, num, true);
+  }
+
+  public void repeat(String str) {
+    repeat(str, 3);
+  }
 }
--- src/main/kotlin/lec_08/Main.kt
+++ src/main/kotlin/lec_08/Main.kt
@@ -1,7 +1,15 @@
 package lec_08
 
 fun main() {
+    // 파라미터가 있는 경우, 해당 파라미터값을 사용한다.
+    repeat("Hello World", 3, false)
 
+    /**
+     * 중간 파라미터는 기본값을 사용하고, 그 다음부터 셋팅하고 싶은 경우
+     * 해당 파라미터를 명시적으로 표시하여 값을 넘긴다.
+     * 이를 'named argument'라고 한다.
+     */
+    repeat("Hello World", useNewLine = false)
 }
 
 fun maxV1(a: Int, b: Int): Int {
@@ -37,3 +45,19 @@ fun maxV4(a: Int, b: Int) =
 
 // 중괄호 생략 가능
 fun maxV5(a: Int, b: Int) = if (a > b) a else b
+
+
+// 기본값을 셋팅하여, 해당 파라미터가 없는 경우 기본값을 사용할 수 있다.
+fun repeat(
+    str: String,
+    num: Int = 3,
+    useNewLine: Boolean = true
+) {
+    for (i in 1..num) {
+        if (useNewLine) {
+            println(str)
+        } else {
+            print(str)
+        }
+    }
+}
