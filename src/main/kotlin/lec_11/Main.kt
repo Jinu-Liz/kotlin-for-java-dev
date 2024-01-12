7e858cf3474ddc4bca2a60728e35842d098be776
Hyungseock-Yun <bagzein@gmail.com>
2024-01-12 23:59:22 +0900

    - 코틀린에서 접근 제어를 다루는 방법 > java와 함께 사용 시 주의할 점

--- src/main/kotlin/lec_11/Main.kt
+++ src/main/kotlin/lec_11/Main.kt
@@ -21,3 +21,13 @@ class Cat public constructor()  // public이 생략되어 있음. 다른 접근 
  * internal - 같은 모듈에서만 접근 가능
  * private - 같은 파일에서만 접근 가능
  */
+
+
+/**
+ * java와 kotlin을 함께 사용할 때 주의할 점
+ * kotlin의 internal의 경우, 바이트 코드에서 public으로 변환되므로
+ * java에서 kotlin 모듈의 internal 코드를 가져올 수 있다.
+ *
+ * kotlin의 protected와 java의 protected는 다르다. (kotlin에서는 같은 패키지에서만의 접근이라는 개념이 없다.)
+ * 따라서, java는 같은 패키지에서의 kotlin protected 멤버에만 접근 가능하다.
+ */
