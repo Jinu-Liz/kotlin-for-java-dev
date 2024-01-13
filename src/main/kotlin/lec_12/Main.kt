25dde93c8d5172e5679b076b142d25b153841a1f
Hyungseock-Yun <bagzein@gmail.com>
2024-01-13 23:40:40 +0900

    - 코틀린에서 object 키워드를 다루는 방법 > singleton 사용

--- src/main/kotlin/lec_12/Main.kt
+++ src/main/kotlin/lec_12/Main.kt
@@ -2,4 +2,14 @@ package lec_12
 
 fun main() {
 
+    println(Singleton.a)
+    Singleton.a += 10
+
+    println(Singleton.a)
+}
+
+
+// kotlin에서 singleton을 사용하는 방법.
+object Singleton {
+    var a: Int = 0
 }
