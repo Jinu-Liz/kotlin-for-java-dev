6053cc15116c64e7d5ecdd8bf7a4892daca24b7b
hsyoon <bagzein@gmail.com>
2024-01-14 14:47:22 +0900

    - 코틀린에서 배열과 컬렉션을 다루는 방법 > Array / List

---
+++ src/main/kotlin/lec_15/Main15.java
@@ -0,0 +1,15 @@
+package lec_15;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main15 {
+  public static void main(String[] args) {
+    int[] array = {100, 200};
+    for (int i = 0; i < array.length; i++) {
+      System.out.printf("%s %s", i, array[i]);
+    }
+
+    final List<Integer> numbers = Arrays.asList(100, 200);
+  }
+}
---
+++ src/main/kotlin/lec_15/Main15.kt
@@ -0,0 +1,46 @@
+package lec_15
+
+fun main() {
+
+    // -- 배열 -- //
+    val array = arrayOf(100, 200)
+    array.plus(300)     // 배열에 element 추가
+
+    for (i in array.indices) {  // indices = 0 ~ lastIndex 까지 Range를 만들어줌
+        println("${i} ${array[i]}")
+    }
+
+    for ((idx, value) in array.withIndex()) {
+        println("${idx} ${value}")
+    }
+
+
+    // -- list -- //
+    /**
+     * 우선 불변 list를 만들고, 필요한 경우에 가변 리스트로 변경하는 것이 좋다.
+     * 코드도 깔끔해지고, 불필요한 가변 리스트 사용이 줄어들기 때문.
+     */
+    val numbers = listOf(100, 200)  // list의 경우, 기본적으로 불변(immutable)이다.
+    val numbers2 = mutableListOf(100, 200)  // 따라서 가변 list를 만들려면 mutable list로 만들어주어야한다. (기본 구현체: ArrayList)
+    numbers.add(300)    // 불변이기 때문에 추가할 수 없음.
+    numbers2.add(300)   // 가변이기 때문에 추가 가능.
+
+    val emptyList = emptyList<Int>()    // element 타입을 지정해주어야 함.
+
+    // printNumbers의 매개변수에 타입이 지정되어 있으므로 별도로 타입을 지정해주지 않아도 자동으로 추론
+    printNumbers(emptyList())
+
+    // 값을 가져올 때는 두가지 모두 가능
+    println(numbers.get(0))
+    println(numbers[0])
+
+    // for문의 경우, 배열과 동일
+    for (number in numbers) println(number)
+
+    for ((idx, value) in numbers.withIndex()) println("$idx $value")
+
+}
+
+private fun printNumbers(numbers: List<Int>) {
+
+}
